27d6f1673877569a335dea940ab1ca4e0e5693c7
Tirumarai Selvan <tirumarai.selvan@gmail.com>
2017-03-24 12:20:57 +0530

    reschedule

--- find-tags.rb
+++ find-tags.rb
@@ -4,11 +4,13 @@ require 'net/http'
 require 'byebug'
 require 'json'
 require 'env'
+require 'active_support'
+require 'active_support/core_ext/numeric/time'
 
 class DescriptionTagFinder
     include Sidekiq::Worker
 
-    def perform( screen_name = "HasuraHQ", cursor = -1)
+    def perform( screen_name = "HasuraHQ", search_tag = "HasuraHQ", cursor = -1)
 
         auth_uri = URI('https://api.twitter.com/oauth2/token')
 
@@ -16,7 +18,7 @@ class DescriptionTagFinder
         auth_req.set_form_data(grant_type: 'client_credentials')
 
         auth_req['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8'
-        auth_req['Authorization'] = ENV["TWITTER_OAUTH"]
+        auth_req['Authorization'] = "Basic #{ENV["TWITTER_OAUTH"]}"
 
         if ENV["TWITTER_OAUTH"].nil? || ENV["TWITTER_OAUTH"].empty?
             raise "TWITTER_OAUTH creds not set in env"
@@ -36,6 +38,8 @@ class DescriptionTagFinder
 
         api = 'https://api.twitter.com/1.1/followers/list.json?skip_status=true&screen_name=' + screen_name
 
+        outFile = File.new(screen_name + "_" + search_tag, "a+")
+
         while cursor != 0 do
             api_with_cursor = api + '&cursor=' + cursor.to_s
             uri = URI(api_with_cursor)
@@ -48,6 +52,7 @@ class DescriptionTagFinder
 
             if res.code == "429"
                 puts "Rate limited exceeded, cursor at: " + cursor.to_s
+                self.class.perform_in(16.minutes, screen_name, search_tag, cursor)
                 break
             end
             res = JSON.parse(res.body)
@@ -56,9 +61,12 @@ class DescriptionTagFinder
             res["users"].each do |user|
                 if user["description"].include? screen_name
                     puts user["screen_name"]
+                    outFile.syswrite(user["screen_name"] + "\n")
                 end
             end
         end
+
+        outFile.close
 
     end
 end
