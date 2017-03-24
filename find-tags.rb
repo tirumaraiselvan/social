77cccd476a442aee4c4f59d2acc0cfa62a5b5cee
Tirumarai Selvan <tirumarai.selvan@gmail.com>
2017-03-24 10:28:12 +0530

    configure sidekiq with ui

--- find-tags.rb
+++ find-tags.rb
@@ -1,50 +1,64 @@
-
+require 'sidekiq'
 require 'uri'
 require 'net/http'
 require 'byebug'
 require 'json'
+require 'env'
 
-screen_name = "HasuraHQ"
+class DescriptionTagFinder
+    include Sidekiq::Worker
 
-auth_uri = URI('https://api.twitter.com/oauth2/token')
+    def perform( screen_name = "HasuraHQ", cursor = -1)
 
-auth_req = Net::HTTP::Post.new(auth_uri)
-auth_req.set_form_data(grant_type: 'client_credentials')
+        auth_uri = URI('https://api.twitter.com/oauth2/token')
 
-auth_req['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8'
-auth_req['Authorization'] = 'Basic xyz'
+        auth_req = Net::HTTP::Post.new(auth_uri)
+        auth_req.set_form_data(grant_type: 'client_credentials')
 
-res = Net::HTTP.start(auth_uri.hostname, auth_uri.port, :use_ssl => true) do |http|
-    http.request(auth_req)
-    end
+        auth_req['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8'
+        auth_req['Authorization'] = ENV["TWITTER_OAUTH"]
 
-res = JSON.parse(res.body)
-
-token = res["access_token"]
-
-cursor = 1524727380465794180
-api = 'https://api.twitter.com/1.1/followers/list.json?skip_status=true&screen_name=' + screen_name
-
-while cursor != 0 do
-    api_with_cursor = api + '&cursor=' + cursor.to_s
-    uri = URI(api_with_cursor)
-    api_req = Net::HTTP::Get.new(uri)
-    api_req['Authorization'] = "Bearer " + token
-
-    res = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => true) do |http|
-            http.request(api_req)
+        if ENV["TWITTER_OAUTH"].nil? || ENV["TWITTER_OAUTH"].empty?
+            raise "TWITTER_OAUTH creds not set in env"
         end
 
-    if res.code == "429"
-        puts "Rate limited exceeded, cursor at: " + cursor.to_s
-        break
-    end
-    res = JSON.parse(res.body)
-    cursor = res["next_cursor"]
+        res = Net::HTTP.start(auth_uri.hostname, auth_uri.port, :use_ssl => true) do |http|
+            http.request(auth_req)
+            end
 
-    res["users"].each do |user|
-        if user["description"].include? screen_name 
-            puts user["screen_name"]
+        unless res.kind_of? Net::HTTPSuccess
+            raise "Oauth error! HTTP code: #{res.code} , body: #{res.body}"
         end
+
+        res = JSON.parse(res.body)
+
+        token = res["access_token"]
+
+        api = 'https://api.twitter.com/1.1/followers/list.json?skip_status=true&screen_name=' + screen_name
+
+        while cursor != 0 do
+            api_with_cursor = api + '&cursor=' + cursor.to_s
+            uri = URI(api_with_cursor)
+            api_req = Net::HTTP::Get.new(uri)
+            api_req['Authorization'] = "Bearer " + token
+
+            res = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => true) do |http|
+                    http.request(api_req)
+                end
+
+            if res.code == "429"
+                puts "Rate limited exceeded, cursor at: " + cursor.to_s
+                break
+            end
+            res = JSON.parse(res.body)
+            cursor = res["next_cursor"]
+
+            res["users"].each do |user|
+                if user["description"].include? screen_name
+                    puts user["screen_name"]
+                end
+            end
+        end
+
     end
 end
